eff8acb8080e966ec428dd16dbca3890886957ae
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-01-01 17:38:59 -0500

    Improve Sonar class from Chapter1

--- src/Chapter2/Sonar.java
+++ src/Chapter2/Sonar.java
@@ -9,7 +9,7 @@ public class Sonar {
     public enum Medium {
         // Values are approximate.
         AIR_SEA_LEVEL_15C (340.0),
-        AIR_JET_ALTITUDE_N57C (295.0),
+        AIR_JET_ALTITUDE_n57C (295.0),
         FRESH_WATER_25C (1497.0),
         SEA_WATER (1500.0);
 
@@ -36,15 +36,17 @@ public class Sonar {
         double seconds = 35.079;
         boolean isEcho = true;
 
-        Sonar sonar1 = new Sonar(Sonar.Medium.AIR_SEA_LEVEL_15C, isEcho);
+        Sonar sonar$AIR_SEA_LEVEL_15C = new Sonar(Sonar.Medium.AIR_SEA_LEVEL_15C, isEcho);
         String verb1 = isEcho ? "echo" : "travel";
-        System.out.format("The sound took %s seconds to %s and thus the distance is %s meters.%n",
-                seconds, verb1, sonar1.distance(seconds));
+        System.out.format("Medium: %s | The sound took %s seconds to %s and thus the distance is %s meters.%n",
+                Sonar.Medium.AIR_SEA_LEVEL_15C.name(), seconds, verb1, sonar$AIR_SEA_LEVEL_15C.distance(seconds));
 
-        Sonar sonar2 = new Sonar(Sonar.Medium.FRESH_WATER_25C, isEcho);
+        seconds = 3.0084;
+        isEcho = false;
+        Sonar sonar$FRESH_WATER_25C = new Sonar(Sonar.Medium.FRESH_WATER_25C, isEcho);
         String verb2 = isEcho ? "echo" : "travel";
-        System.out.format("The sound took %s seconds to %s and thus the distance is %s meters.%n",
-                seconds, verb2, sonar2.distance(seconds));
+        System.out.format("Medium: %s | The sound took %s seconds to %s and thus the distance is %s meters.%n",
+                Sonar.Medium.FRESH_WATER_25C.name(), seconds, verb2, sonar$FRESH_WATER_25C.distance(seconds));
     }
 }
 
