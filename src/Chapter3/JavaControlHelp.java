be1f9026777ee1f8bdde457bf7881c0a6f4c93e0
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-01-04 21:58:06 -0500

    Finish all showHelp methods.

--- src/Chapter3/JavaControlHelp.java
+++ src/Chapter3/JavaControlHelp.java
@@ -15,18 +15,18 @@ public class JavaControlHelp {
     private void startJavaControlHelp() {
         System.out.format(
                 "** Welcome to Java Control Help! **%n%n" +
-                "Please choose one of the following to learn more about Java constrol structures:%n%n"
+                "Please choose one of the following to learn more about Java control structures:%n%n"
         );
         showSelections();
     }
     private void showSelections() {
         System.out.format(
                 "Help on:%n" +
-                "\tA. `if/else` control%n" +
-                "\tB. `switch` control%n" +
-                "\tC. `for` control%n" +
-                "\tD. `while` control%n" +
-                "\tE. `do-while` control%n"
+                "\tA. `if/else`%n" +
+                "\tB. `switch`%n" +
+                "\tC. `for` loop%n" +
+                "\tD. `while` loop%n" +
+                "\tE. `do-while loop`%n"
         );
         acquireSelection();
     }
@@ -40,13 +40,21 @@ public class JavaControlHelp {
         }
     }
     private void routeSelectionToHelpItem(char selection) {
-        // TODO add methods for selections B-E
         switch (selection) {
             case 'A':
                 showHelpFor_IfElse();
                 break;
             case 'B':
                 showHelpFor_Switch();
+                break;
+            case 'C':
+                showHelpFor_ForLoop();
+                break;
+            case 'D':
+                showHelpFor_WhileLoop();
+                break;
+            case 'E':
+                showHelpFor_DoWhileLoop();
                 break;
             default:
                 System.out.format("Selection \'%c\' was not recognized. Please try again%n", selection);
@@ -95,17 +103,42 @@ public class JavaControlHelp {
     }
     private void showHelpFor_Switch() {
         System.out.format(
-                "Switch control structure:%n" +
-                "\tswitch (expression) {%n" +
-                "\t  case X:%n" +
+                "`switch` control structure:%n" +
+                "\tswitch (expressionToEvaluate) {%n" +
+                "\t  case valueX:%n" +
                 "\t    //do something%n" +
                 "\t    break;%n" +
-                "\t  case Y:%n" +
+                "\t  case valueY:%n" +
                 "\t    //do something different%n" +
                 "\t    break;%n" +
                 "\t  default:%n" +
                 "\t    //do this if no other condition matches%n" +
                 "\t}%n"
+        );
+    }
+    private void showHelpFor_ForLoop() {
+        System.out.format(
+                "`for` loop control structure:%n" +
+                "\tfor (int i = 0; i < 10; i++) {%n" +
+                "\t    //do something 10 times%n" +
+                "\t}%n"
+        );
+    }
+    private void showHelpFor_WhileLoop() {
+        System.out.format(
+                "`while` loop control structure:%n" +
+                "\twhile (someCondition) {%n" +
+                "\t    //do something while someCondition is true%n" +
+                "\t}%n"
+        );
+    }
+    private void showHelpFor_DoWhileLoop() {
+        System.out.format(
+                "`do-while` loop control structure:%n" +
+                "\tdo {%n" +
+                "\t    //do something at least once%n" +
+                "\t    //and while someCondition is true%n" +
+                "\t} while (someCondition)%n"
         );
     }
 
