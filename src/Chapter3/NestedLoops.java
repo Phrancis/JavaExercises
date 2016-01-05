7717009ece068937145e73cf19d8a3dce2724975
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-01-05 01:57:25 -0500

    Done with Chapter3

--- src/Chapter3/NestedLoops.java
+++ src/Chapter3/NestedLoops.java
@@ -1,7 +1,20 @@
 package Chapter3;
 
-/**
- * Created by francisveilleux-gaboury on 1/5/16.
- */
 public class NestedLoops {
+
+    private static void findFactorials(int start, int limit) {
+            for (int i = start ; i <= limit; i++) {
+                System.out.format("%nFactorials of %d: ", i);
+                for (int j = start; j <= i; j++) {
+                    if (i != j && i % j == 0) {
+                        System.out.format("%d ", j);
+                    }
+                }
+            }
+    }
+
+    public static void main (String[] args) {
+        System.out.format("NestedLoops%n");
+        findFactorials(2, 500);
+    }
 }
