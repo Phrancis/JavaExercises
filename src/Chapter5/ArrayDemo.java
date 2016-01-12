4cbc81415a8dfb96de9e17946b479a9e7eef113a
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-01-11 19:30:59 -0500

    Moved bubblesort into its own class

--- src/Chapter5/ArrayDemo.java
+++ src/Chapter5/ArrayDemo.java
@@ -37,44 +37,11 @@ public class ArrayDemo {
             System.out.format("%s ", word);
         }
     }
-    static void arrayBubbleSort(int nums) {
-        System.out.println("arrayBubbleSort()");
-        int[] randomInts = RandomIntArray.generate(nums);
-        int iterator, comparator, temp;
-        final int SIZE = randomInts.length;
-        for (iterator = 1; iterator < SIZE; iterator++) {
-            //Start from last index and loop backwards
-            for (comparator = SIZE - 1; comparator >= iterator; comparator--) {
-                // if out of order...
-                if (randomInts[comparator - 1] > randomInts[comparator]) {
-                    // exchange elements:
-                    temp = randomInts[comparator - 1];
-                    randomInts[comparator - 1] = randomInts[comparator];
-                    randomInts[comparator] = temp;
-                }
-            }
-        }
-//        System.out.println("Sorted array:");
-//        for (int i = 0; i < randomInts.length; i++) {
-//            System.out.format("randomInts[%d] : %d%n", i, randomInts[i]);
-//        }
-    }
 
     public static void main(String[] args) {
         System.out.println("**ArrayDemo**");
-        //squaresArray(13);
-        //minAndMaxInArray(10);
-        //arrayWithInitializer();
-        /**execution time of arrayBubbleSort()*/
-        long startTime = System.currentTimeMillis();
-        arrayBubbleSort(10000);
-        long endTime = System.currentTimeMillis();
-        System.out.format("arrayBubbleSort() duration: %d ms.%n", endTime - startTime);
-        /**comparison with java.util.Arrays.sort()*/
-        startTime = System.currentTimeMillis();
-        int[] randomInts = RandomIntArray.generate(10000);
-        Arrays.sort(randomInts);
-        endTime = System.currentTimeMillis();
-        System.out.format("Arrays.sort() duration: %d ms.%n", endTime - startTime);
+        squaresArray(13);
+        minAndMaxInArray(10);
+        arrayWithInitializer();
     }
 }
