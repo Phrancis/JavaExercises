4b26e90ed219910a8158b04518bc73f9b5ad46ed
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-03-30 07:54:49 -0400

    Add Block class to illustrate passing objects to methods

---
+++ src/Chapter6/BlockDemo.java
@@ -0,0 +1,26 @@
+package Chapter6;
+
+/**
+ * Demonstrate the use of Block and passing objects to methods.
+ */
+public class BlockDemo {
+    public static void main(String[] args) {
+        Block block1 = new Block(3,4,5);
+        System.out.printf("block1 volume: %d%n", block1.getVolume());
+        Block block2 = new Block(4,5,3);
+        System.out.printf("block2 volume: %d%n", block2.getVolume());
+        System.out.println("block1 is same as block2? " + block1.isSameBlock(block2));
+        System.out.println("block1 has same volume as block2? " + block1.hasSameVolume(block2));
+        //
+        block1.setWidth(4);
+        block1.setHeight(2);
+        block1.setDepth(6);
+        System.out.printf("block1 new width %d, new height %d, new depth %d and new volume is %d%n",
+                block1.getWidth(), block1.getHeight(), block1.getDepth(), block1.getVolume());
+        System.out.println("block1 is same as block2? " + block1.isSameBlock(block2));
+        System.out.println("block1 has same volume as block2? " + block1.hasSameVolume(block2));
+        Block block3 = new Block(4, 2, 6);
+        System.out.println("block1 is same as block3? " + block1.isSameBlock(block3));
+        System.out.println("block1 has same volume as block3? " + block1.hasSameVolume(block3));
+    }
+}
