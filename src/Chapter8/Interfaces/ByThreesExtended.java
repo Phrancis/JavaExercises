7acd66ad7d9366f21d59300334022ad4b8439700
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-04-02 21:12:30 -0400

    Add SeriesExtended, and an implementation

---
+++ src/Chapter8/Interfaces/ByThreesExtended.java
@@ -0,0 +1,23 @@
+package Chapter8.Interfaces;
+
+/**
+ * Implements additional method from SeriesExtended interface.
+ */
+public class ByThreesExtended extends ByThrees implements SeriesExtended {
+
+    @Override
+    public int[] getNext3() {
+        int[] ints = new int[3];
+        ints[0] = super.getNext();
+        ints[1] = super.getNext();
+        ints[2] = super.getNext();
+        return ints;
+    }
+    // Demo
+    public static void main(String[] args) {
+        ByThreesExtended b = new ByThreesExtended();
+        System.out.println(b.getNext());
+        int[] nums = b.getNext3();
+        for(int i : nums) System.out.print(i + " ");
+    }
+}
