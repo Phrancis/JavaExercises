fd6972d13d817c1f5e30390044ee0cd08cf8383c
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-04-05 00:22:58 -0400

    Add CompareFiles

---
+++ src/Chapter10/CompareFiles.java
@@ -0,0 +1,42 @@
+package Chapter10;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Try This 10-1: A file comparison utility
+ */
+public class CompareFiles {
+    public static void compareFiles(String file1, String file2) {
+        int ix1 = 0, ix2 = 0;
+        try(FileInputStream fileIn1 = new FileInputStream(file1);
+            FileInputStream fileIn2 = new FileInputStream(file2))
+        {
+            do {
+                ix1 = fileIn1.read();
+                ix2 = fileIn2.read();
+                if(ix1 != ix2) {
+                    break;
+                }
+            } while (ix1 != -1 && ix2 != -1);
+            if(ix1 != ix2) {
+                System.out.println("Files are different.");
+            }
+            else {
+                System.out.println("Files are the same.");
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // DEMO
+    public static void main(String[] args) {
+        String file1 = "/Users/francisveilleux-gaboury/Documents/demo.txt";
+        String file2 = "/Users/francisveilleux-gaboury/Documents/demo2.txt";
+        String file3 = "/Users/francisveilleux-gaboury/Documents/demo3.txt";
+        CompareFiles.compareFiles(file1, file2);
+        CompareFiles.compareFiles(file1, file3);
+        CompareFiles.compareFiles(file2, file3);
+    }
+
+}
---
+++ src/Chapter10/WriteAndReadData.java
@@ -0,0 +1,39 @@
+package Chapter10;
+
+import java.io.*;
+
+/**
+ * Write then read back binary data.
+ */
+public class WriteAndReadData {
+    static int i = 42;
+    static double d = 3.14159;
+    static boolean b = true;
+
+    public static void writeData() {
+        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("testdata"))) {
+            System.out.println("Writing " + i);
+            dataOut.writeInt(i);
+            System.out.println("Writing " + d);
+            dataOut.writeDouble(d);
+            System.out.println("Writing " + b);
+            dataOut.writeBoolean(b);
+        }
+        catch (IOException exc) {
+            System.out.println(exc);
+        }
+    }
+    public static void readData() {
+        try(DataInputStream dataIn = new DataInputStream(new FileInputStream("testdata"))) {
+            i = dataIn.readInt();
+
+        }
+        catch (IOException exc){
+            System.out.println(exc);
+        }
+    }
+    // DEMO
+    public static void main(String[] args) {
+        WriteAndReadData.writeData();
+    }
+}
