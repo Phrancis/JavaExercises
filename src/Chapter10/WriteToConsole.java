9da71921f8fe673d729f87e24e9681248e661188
Francis Veilleux-Gaboury <djphrancis@gmail.com>
2016-04-05 01:40:30 -0400

    Add WriteToConsole

---
+++ src/Chapter10/WriteToConsole.java
@@ -0,0 +1,16 @@
+package Chapter10;
+
+import java.io.PrintWriter;
+
+/**
+ * Print to console using PrintWriter
+ */
+public class WriteToConsole {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out, true);
+        pw.println("Printing to console:");
+        pw.print("Hello, ");
+        pw.print("world!\n");
+        pw.printf("%d + %d = %d", 2,3,2+3);
+    }
+}
