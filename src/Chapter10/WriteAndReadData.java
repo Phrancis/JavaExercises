aaa51873d5da97b4a1c74f5ac37f16ff6a80776b
Phrancis <music@psychoproductions.net>
2016-04-10 18:43:40 -0400

    improve WriteAndReadData

--- src/Chapter10/WriteAndReadData.java
+++ src/Chapter10/WriteAndReadData.java
@@ -6,12 +6,12 @@ import java.io.*;
  * Write then read back binary data.
  */
 public class WriteAndReadData {
-    static int i = 42;
-    static double d = 3.14159;
-    static boolean b = true;
 
-    public static void writeData() {
-        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("testdata"))) {
+    private void writeData() {
+        try (DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("D:/Downloads/testdata.txt"))) {
+            int i = 42;
+            double d = 3.14159;
+            boolean b = true;
             System.out.println("Writing " + i);
             dataOut.writeInt(i);
             System.out.println("Writing " + d);
@@ -20,20 +20,26 @@ public class WriteAndReadData {
             dataOut.writeBoolean(b);
         }
         catch (IOException exc) {
-            System.out.println(exc);
+            exc.printStackTrace();
         }
     }
-    public static void readData() {
-        try(DataInputStream dataIn = new DataInputStream(new FileInputStream("testdata"))) {
-            i = dataIn.readInt();
-
+    private void readData() {
+        try(DataInputStream dataIn = new DataInputStream(new FileInputStream("D:/Downloads/testdata.txt"))) {
+            int i = dataIn.readInt();
+            System.out.printf("Read: %d%n", i);
+            double d = dataIn.readDouble();
+            System.out.printf("Read: %f%n", d);
+            boolean b = dataIn.readBoolean();
+            System.out.printf("Read: %s%n", b);
         }
         catch (IOException exc){
-            System.out.println(exc);
+            exc.printStackTrace();
         }
     }
     // DEMO
     public static void main(String[] args) {
-        WriteAndReadData.writeData();
+        WriteAndReadData wr = new WriteAndReadData();
+        wr.writeData();
+        wr.readData();
     }
 }
